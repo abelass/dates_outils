6fb2e4353883ad3ae4acacdc6cb23769374edd09
abelass <rainer@websimple.be>
2018-05-28 10:21:56 +0200

    la fabrique

---
+++ dates_outils_fonctions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Fonctions utiles au plugin Dates outils
+ *
+ * @plugin     Dates outils
+ * @copyright  2018
+ * @author     Rainer Müller
+ * @licence    GNU/GPL v3
+ * @package    SPIP\Dates_outils\Fonctions
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
