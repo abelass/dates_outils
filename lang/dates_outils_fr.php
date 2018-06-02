0e177f501da4b3fd936f09cc4c26a143509b4dbb
Rainer Müller <rainer@websimple.be>
2018-06-02 12:52:36 +0200

    chainee de langue date

--- lang/dates_outils_fr.php
+++ lang/dates_outils_fr.php
@@ -8,6 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
+	'champ_date_label' => 'Date',
 	'champ_date_debut_label' => 'Date début',
 	'champ_date_fin_label' => 'Date fin',
 
