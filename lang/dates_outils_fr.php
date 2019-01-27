ee815349d6b2bc0ed73cf5a8991d9536134ee38e
Rainer Müller <rainer@websimple.be>
2019-01-27 12:21:35 +0100

    des chaînes additionnels

--- lang/dates_outils_fr.php
+++ lang/dates_outils_fr.php
@@ -2,22 +2,29 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 if (!defined('_ECRIRE_INC_VERSION')) {
-	return;
+  return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// C
-	'champ_date_label' => 'Date :',
-	'champ_date_debut_label' => 'Date début :',
-	'champ_date_fin_label' => 'Date fin :',
+  // C
+  'champ_annee_label' => 'Année :',
+  'champ_date_label' => 'Date :',
+  'champ_date_debut_label' => 'Date début :',
+  'champ_date_fin_label' => 'Date fin :',
 
-	// D
-	'date_au' => 'au',
-	'date_du' => 'du',
-	'dates_outils_titre' => 'Dates outils',
+  // D
+  'date_au' => 'au',
+  'date_du' => 'du',
+  'dates_outils_titre' => 'Dates outils',
 
-	// P
-	'periode_label' => 'Période :',
+  // J
+  'champ_jour_label' => 'Jour :',
+
+  // M
+  'champ_mois_label' => 'Mois :',
+
+  // P
+  'periode_label' => 'Période :',
 
 );
